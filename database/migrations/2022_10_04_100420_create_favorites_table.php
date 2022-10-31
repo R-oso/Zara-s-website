6f5986109506dea1db2fc15d76d12c2c181eec1e
R-oso <luukroso@gmail.com>
2022-10-31 23:01:56 +0100

    Created favorites table

--- database/migrations/2022_10_04_100420_create_favorites_table.php
+++ database/migrations/2022_10_04_100420_create_favorites_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateLikesTable extends Migration
+class CreateFavoritesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,14 +13,12 @@ class CreateLikesTable extends Migration
      */
     public function up()
     {
-        Schema::create('likes', function (Blueprint $table) {
+        Schema::create('favorites', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained()->onDelete('cascade');
             $table->foreignId('project_id')->constrained()->onDelete('cascade');;
-            $table->boolean('liked');
+            $table->boolean('favored')->nullable();
             $table->timestamps();
-
-            $table->unique(['user_id', 'project_id']);
         });
     }
 
@@ -31,6 +29,6 @@ class CreateLikesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('likes');
+        Schema::dropIfExists('favorites');
     }
 }
